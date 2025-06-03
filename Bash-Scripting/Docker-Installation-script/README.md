d6b6fd3fa248eaa8cc2caf7dc42866ee3888bbf6
Dinesh <nbz27@akr.com>
2025-06-03 18:45:27 +0530

    Docker Installation script for debian, centos, and suse Distribution

---
+++ Bash-Scripting/Docker-Installation-script/README.md
@@ -0,0 +1,11 @@
+# Docker Installation Script
+
+## Description,
+   This script will install official **docker engine** and **docker compose** individually for ubuntu, cent os, debian, and centos distribution
+
+## Flow Chart
+
+[FlowChart](assets/docker%20installation%20script.svg)
+
+
+## Execution 
---
+++ Bash-Scripting/Docker-Installation-script/docker-installer.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+
+declare -a ubUnoffPackages=(
+    [0]=docker.io
+    [1]=docker-compose
+    [2]=docker-compose-v2
+    [3]=docker-doc
+    [4]=podman-docker
+)
+# 0: false 1: true
+isInteractive=1
+
+declare -a rhUnoffPackages=(
+    [0]=docker
+    [1]=docker-client
+    [2]=docker-client-latest
+    [3]=docker-common
+    [4]=docker-logrotate
+    [5]=docker-latest-logrotate
+    [7]=podman
+    [6]=docker-latest
+    [8]=runc
+    [9]=docker-engine
+)
+
+declare -a suUnoffpackages=(
+    [0]=docker
+)
+
+getDistribution () {
+    echo $(grep ^NAME /etc/os-release | cut -d "=" -f 2 | tr -d '"')
+}
+
+rmUnoffPackage () {
+    if [ "$1" = "ub" ]; then
+        for pkg in ${ubUnoffPackages[@]}; do
+          sudo apt-get remove $pkg
+        done;
+    elif [ "$1" = "rh" ]; then
+        for pkg in ${rhUnoffPackages[@]}; do  
+          sudo yum remove $pkg
+        done;
+    elif [ "$1" = "su" ]; then
+        for pkg in ${suUnoffpackages[@]}; do
+          sudo zypper remove $pkg 
+        done;
+    else 
+        echo "ERR: Unable to remove the package"
+    fi;
+}
+
+ubuntuDistribution () {
+    echo "WARN: Removing un official package"
+    rmUnoffPackage ub
+    echo "INFO: Adding the repository"
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+       $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    
+    echo "INFO: Installing docker engine."
+
+    sudo apt-get install $(if [ $isInteractive -eq 0 ]; then echo "-y"; fi) docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin  
+    
+}
+
+redhatDistribution () {
+    echo "INFO: Removing un official package"
+    rmUnoffPackage rh
+    echo "Adding Repo"
+    sudo dnf install -y dnf-plugins-core
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+    
+}
+
+suseDistribution() {
+    echo "WARN: Removing un official package"
+    rmUnoffPackage su
+    echo "INFO: adding the repository"
+
+}
+
+main () {
+    distribution=$(getDistribution)
+    # if [$distribution -eq "Red Hat Enterprise Linux"]; then
+    case $distribution in
+       "Ubuntu")
+          ubuntuDistribution
+          ;;
+        "Red Hat Enterprise Linux")
+          redhatDistribution
+          ;;
+        "Suse")
+           suseDistribution
+          ;;
+        *)
+          echo "ERR: script doesn't support $distribution distribution"
+          ;;
+    esac
+}
+
+if [[ $# -eq 1 && "$1" = "--non-interactive" ]]; then;
+    isInteractive=0
+fi;
+    
+main
+
